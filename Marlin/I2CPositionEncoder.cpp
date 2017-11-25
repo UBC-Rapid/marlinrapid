5579a4d2c9964af879082d3397f97c9831ba761e
Ian Koh <ian_koh93@hotmail.com>
2017-11-24 16:45:40 -0800

    opps i fucked it

--- Marlin/I2CPositionEncoder.cpp
+++ Marlin/I2CPositionEncoder.cpp
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+//sdfkjgkadgdkaghkdahg
 
 //todo:  add support for multiple encoders on a single axis
 //todo:    add z axis auto-leveling
